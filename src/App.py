40304e5ede8e25fb983e26c7cd8a8ff1ef78fb57
jesimielefraimdias <59701946+jesimielefraimdias@users.noreply.github.com>
2020-12-30 23:05:23 -0300

    Update App.py

--- src/App.py
+++ src/App.py
@@ -7,7 +7,8 @@ app.secret_key = "Secret Key"
 #SqlAlchemy Database Configuration With Mysql
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@127.0.0.1:3306/slice2'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:computacao@127.0.0.1:3306/slice2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:computacao@localhost:3306/slice2'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:computacao@localhost:3306/slice2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:computacao@0.0.0.0:3306/slice2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
